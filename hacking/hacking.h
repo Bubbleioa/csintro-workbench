ca011972d07aec381feff2b09674cc5438db27ad
Bubbleioa <1342130847@qq.com>
2022-08-30 20:46:56 +0800

    lab1: remove extern

--- hacking/hacking.h
+++ hacking/hacking.h
@@ -8,9 +8,9 @@
 #include <stdbool.h>
 #include <string.h>
 
-extern unsigned char *fetch_mem(uint64_t ch_addr, int length, int *read_btyes, bool is_first_scan);
+unsigned char *fetch_mem(uint64_t ch_addr, int length, int *read_btyes, bool is_first_scan);
 
-extern void write_mem(uint64_t ch_addr, unsigned char *ch, int length);
+void write_mem(uint64_t ch_addr, unsigned char *ch, int length);
 
 int init_scan();
 
